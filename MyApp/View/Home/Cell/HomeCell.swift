611057ee3340d971ff9e735591be78f54c7a1022
Hanh Nguyen H <hanh.nguyen3@asiantech.vn>
2020-02-07 17:08:53 +0700

    Make home cell.

---
+++ MyApp/View/Home/Cell/HomeCell.swift
@@ -0,0 +1,19 @@
+//
+//  HomeCell.swift
+//  MyApp
+//
+//  Created by ANH NGUYỄN on 2/7/20.
+//  Copyright © 2020 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+final class HomeCell: UITableViewCell {
+
+    @IBOutlet weak var videoImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
