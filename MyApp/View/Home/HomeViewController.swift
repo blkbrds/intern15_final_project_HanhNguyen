651d9ac9e7e05e28a320029f7184b2881238fb93
ANH NGUYỄN <hanh.nguyen3@asiantech.vn>
2020-02-07 14:26:53 +0700

    Add View Controler

---
+++ MyApp/View/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  MyApp
+//
+//  Created by ANH NGUYỄN on 2/7/20.
+//  Copyright © 2020 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
