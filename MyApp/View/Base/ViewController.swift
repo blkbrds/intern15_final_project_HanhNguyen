d53b6b87a51b383112a7192ad252f3334e551354
Hanh Nguyen H <hanh.nguyen3@asiantech.vn>
2020-02-10 11:28:48 +0700

    Delete unnecessary.

--- MyApp/View/Base/ViewController.swift
+++ MyApp/View/Base/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     private let scopes = [kGTLRAuthScopeYouTubeReadonly]
     private let service = GTLRYouTubeService()
     let signInButton = GIDSignInButton()
-    let output = UITextView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,37 +27,7 @@ class ViewController: UIViewController {
 
         // Add the sign-in button.
         view.addSubview(signInButton)
-
-        // Add a UITextView to display output.
-        output.frame = view.bounds
-        output.isEditable = false
-        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
-        output.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        output.isHidden = true
-        view.addSubview(output)
     }
-
-    // List up to 10 files in Drive
-        func fetchChannelResource() {
-            let query = GTLRYouTubeQuery_ChannelsList.query(withPart: "snippet,statistics")
-            query.identifier = "UC_x5XG1OV2P6uZZ5FSM9Ttw"
-            service.executeQuery(query,
-                                 delegate: self,
-                                 didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:)))
-        }
-
-        // Process the response and display output
-    @objc func displayResultWithTicket(
-            ticket: GTLRServiceTicket,
-            finishedWithObject response: GTLRYouTube_ChannelListResponse,
-            error: NSError?
-        ) {
-            if let error = error {
-                alert(title: "Error", msg: error.localizedDescription, buttons: ["OK"], handler: nil)
-                return
-            }
-            navigationItem.rightBarButtonItems?.removeFirst()
-        }
 
     func setupUI() {
         let leftView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
@@ -107,11 +76,11 @@ class ViewController: UIViewController {
 extension ViewController: GIDSignInDelegate, GIDSignInUIDelegate {
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
               withError error: Error!) {
+        print("eafewf")
         if error != nil {
             self.service.authorizer = nil
         } else {
             self.service.authorizer = user.authentication.fetcherAuthorizer()
-            fetchChannelResource()
 //            UserDefaults.standard.set(user.authentication.accessToken, forKey: "youtubeAccessToken")
         }
     }
