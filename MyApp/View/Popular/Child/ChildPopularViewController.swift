00c12ecddf75269aae0879ff55aa9d97e9121d59
Hanh Nguyen H <hanh.nguyen3@asiantech.vn>
2020-02-15 18:15:03 +0700

    config popular ui.

---
+++ MyApp/View/Popular/Child/ChildPopularViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ChildPopularViewController.swift
+//  MyApp
+//
+//  Created by ANH NGUYỄN on 2/15/20.
+//  Copyright © 2020 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+final class ChildPopularViewController: ViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var viewModel = ChildPopularViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func setupUI() {
+        tableView.register(name: CellIdentifier.homeCell.rawValue)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+extension ChildPopularViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.homeCell.rawValue, for: indexPath) as? HomeCell else {
+            return UITableViewCell()
+        }
+        cell.viewModel = viewModel.viewModelForCell(at: indexPath)
+        return cell
+    }
+}
+
+extension ChildPopularViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        UITableView.automaticDimension
+    }
+}
--- MyApp/View/Popular/Child/ChildPopularViewModel.swift
+++ MyApp/View/Popular/Child/ChildPopularViewModel.swift
@@ -1,21 +1,14 @@
 //
-//  PopularViewModel.swift
+//  ChildPopularViewModel.swift
 //  MyApp
 //
-//  Created by ANH NGUYỄN on 2/10/20.
+//  Created by ANH NGUYỄN on 2/15/20.
 //  Copyright © 2020 Asian Tech Co., Ltd. All rights reserved.
 //
 
 import Foundation
-enum VideoCategory {
-    case music
-    case game
-    case news
-    case movie
-}
 
-final class PopularViewModel {
-    
+final class ChildPopularViewModel {
     var videos: [Video] = []
     
     func viewModelForCell(at indexPath: IndexPath) -> HomeCellViewModel {
