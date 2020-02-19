56b4b365146a86e0b866b4e359d42c9fb555792e
Hanh Nguyen H <hanh.nguyen3@asiantech.vn>
2020-02-19 11:28:25 +0700

    config UI Favorite.

--- MyApp/View/Library/LibraryViewController.swift
+++ MyApp/View/Library/LibraryViewController.swift
@@ -9,8 +9,35 @@
 import UIKit
 
 final class LibraryViewController: ViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
 
+    var viewModel = LibraryViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func setupUI() {
+        tableView.register(name: CellIdentifier.homeCell.rawValue)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+extension LibraryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+extension LibraryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRowsInSection(at: section)
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.homeCell.rawValue, for: indexPath) as? HomeCell else {
+            return UITableViewCell()
+        }
+        return cell
     }
 }
