969587d641fbc5d67fc1368c9405bc6684ae9a63
Hanh Nguyen H <hanh.nguyen3@asiantech.vn>
2020-02-19 13:59:15 +0700

    handle logic.

--- MyApp/View/Detail/DetialViewModel.swift
+++ MyApp/View/Detail/DetialViewModel.swift
@@ -31,6 +31,7 @@ final class DetailViewModel {
         let params = Api.Detail.CommentParams(part: "snippet", videoId: video.id, key: App.String.apiKey, maxResults: 5, pageToken: pageToken)
         Api.Detail.getComments(params: params) { [weak self] (result) in
             guard let this = self else { return }
+            this.isLoading = false
             switch result {
             case .success(let result):
                 if isLoadMore {
@@ -44,7 +45,6 @@ final class DetailViewModel {
                 completion(.failure(error))
             }
         }
-        isLoading = false
     }
 
     func loadApiVideoDetail(completion: @escaping ApiComletion) {
