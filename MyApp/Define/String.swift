1e81570a643a1ba3d67123a5949d8eb77acfdbdf
Hanh Nguyen H <hanh.nguyen3@asiantech.vn>
2020-02-20 11:32:41 +0700

    Fix conflict ui.

--- MyApp/Define/String.swift
+++ MyApp/Define/String.swift
@@ -17,7 +17,7 @@ extension App {
     struct String {
         static let error = "ERROR"
         static let ok = "OK"
-        static let apiKey = "AIzaSyA790TZ1NAfkubwNHUTOF0F2UfGK1dvKuI"
+        static let apiKey = "AIzaSyBq2DwWIYCWg6vJBHSn2_wtJgZTgJyDFWo"
         static let dateFormatYYYYMMDDTHHmmss = "yyyy-MM-dd'T'HH:mm:ssZ"
         static let dateFormatYYYYMMDDHHmmss = "yyyy-MM-dd HH:mm:ss"
         static let refresh = "Refreshing Data..."
